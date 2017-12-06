ce069433489ce08b3268d4398ca5daf9c0c23dac
zennen <b05502055@csie.ntu.edu.tw>
2017-12-06 10:18:23 +0800

    version 4 (final), solve decrease key problem of priority queue

--- st_noncut.cpp
+++ st_noncut.cpp
@@ -5,14 +5,12 @@
 #include <queue>
 #define ULLMAX 18446744073709551615
 using namespace std;
-
-unsigned long long dist[300001] = {0};
 class myComparator
 {
 public:
-    int operator() (const int& p1, const int& p2)
+    int operator() (const pair<int,unsigned long long>& p1, const pair<int,unsigned long long>& p2)
     {
-        return dist[p1] > dist[p2];
+        return p1.second > p2.second;
     }
 };
 
@@ -20,47 +18,41 @@ int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
+	int n,m,s,t,tmp1,tmp2,tmp3;
 	unsigned long long TOTAL_WEIGHT = 0;
-	int n,m,s,t;
+	
 	cin >> n >> m >> s >> t;	// n: # of nodes, m: # of edges, s to t
 	s--;t--;
 	vector< list< pair<int,int> > >Alist(n);
 	
 	for(int i=0;i<m;i++){
-		int u,v,weight;
-		cin >> u >> v >> weight;
-		TOTAL_WEIGHT += weight;
-		u--;v--;	// array starts from zero, but node start from 1
-		Alist[u].push_back(make_pair(v,weight));
-		Alist[v].push_back(make_pair(u,weight));
+		cin >> tmp1 >> tmp2 >> tmp3;
+		TOTAL_WEIGHT += tmp3;
+		Alist[tmp1-1].push_back(make_pair(tmp2-1,tmp3));
+		Alist[tmp2-1].push_back(make_pair(tmp1-1,tmp3));
 	}
 
-	bool isQ[300001] = {0};
-	priority_queue<int, vector<int>, myComparator > pq;
+	unsigned long long dist[300001] = {0};
+	priority_queue< pair<int,unsigned long long>, vector<pair<int,unsigned long long> >, myComparator > pq;
 	
-	pq.push(s);
-	isQ[s] = 1;
+	pq.push(make_pair(s,dist[s]));
 	for(int i=0;i<n;i++) if(i!=s) dist[i] = ULLMAX;
 	
 	while(!pq.empty()){
-		int u = pq.top();
+		pair<int,unsigned long long> u = pq.top();
 		pq.pop();
-		list< pair<int, int> >::iterator itr = Alist[u].begin();		         
-		while (itr != Alist[u].end()) {
+		list< pair<int, int> >::iterator itr = Alist[u.first].begin();		         
+		while (itr != Alist[u.first].end()) {
+			unsigned long long neW = dist[u.first] + (*itr).second;
 			int v = (*itr).first;
-			int dist_uv = (*itr).second;
-			unsigned long long new_distV = dist[u] + dist_uv;
-			if(new_distV < dist[v]){
-				dist[v] = new_distV;
-				if(isQ[v]==0){
-					isQ[v] = 1;
-					pq.push(v);
-				}
+			if(neW < dist[v]){
+				dist[v] = neW;
+				pq.push(make_pair(v,dist[v]));
 			}
 			++itr;
 		}
 	}
-	if(dist[t]==ULLMAX) cout << "-1";
+	if(dist[t]>TOTAL_WEIGHT) cout << "-1";
 	else cout << TOTAL_WEIGHT - dist[t];
 	cout << endl;
 }

fbec8a23fe2d4e871bde5922e466f83725e8b4c0
zennen <b05502055@csie.ntu.edu.tw>
2017-12-08 01:12:19 +0800

    three roads v3 stackDFS, WA + TLE

--- three_roads.cpp
+++ three_roads.cpp
@@ -1,33 +1,14 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <string.h>
 #include <list>
+#include <stack>
+#include <string.h>
 using namespace std;
 
-bool ary[100000] = {0};	// 1 odd, 2 even
+bool visited[100000] = {0};	// 1 odd, 2 even
 bool cl[100000] = {0};
-unsigned long long pnt=0,edge=0;
 vector< list<int> >Alist(100000); 
-
-bool DFSvisit(int u, bool color){
-	bool rn = false;
-	++pnt;
-	ary[u] = 1;
-	cl[u] = color;
-	list<int>::iterator itr = Alist[u].begin();
-	// true -> all, false -> no odd-even
-	// for(list<int>::iterator itr = Alist[u].begin(); itr!=Alist[u].end(); ++itr){
-	while(itr!=Alist[u].end()){
-		++edge;
-		if(ary[*itr]==0){
-			if(DFSvisit(*itr,!color)==true) rn = true;
-		}
-		else if(cl[*itr]==color) rn  = true;
-		++itr;
-	}
-	return rn;
-}
+stack<int>dfs;
 
 int main(){
 	ios::sync_with_stdio(0);
@@ -35,7 +16,7 @@ int main(){
 	int T;
 	cin >> T;
 	while(T--){
-		memset(ary,0,sizeof ary);
+		memset(visited,0,sizeof visited);
 		unsigned long long TOTAL_ROAD = 0;
 		int N,M,u,v;
 		cin >> N >> M;
@@ -47,16 +28,33 @@ int main(){
 		}
 		// traverse
 		for(int i=0;i<N;++i){
-			if(ary[i]==0){
-				pnt = 0; edge = 0;
-				// all connected
-				if(DFSvisit(i,0)==true)	TOTAL_ROAD += (pnt*(pnt-1)-edge)/2;
-				// seperated
-				else TOTAL_ROAD += (pnt/2)*(pnt/2+pnt%2)-edge/2;
+			if(visited[i]==0){
+				unsigned long long pnt = 0,edge = 0;
+				bool isAll = false;
+				dfs.push(i);
+				cl[i] = 0;
+				while(!dfs.empty()){
+					int u = dfs.top();
+					dfs.pop();
+					if(!visited[u]){
+						visited[u] = true;
+						++pnt;
+						for(list<int>::iterator itr = Alist[u].begin(); itr!=Alist[u].end(); ++itr){
+							if(visited[*itr]==0){
+								++edge;
+								dfs.push(*itr);
+								cl[*itr] = !cl[u];
+							}
+							else if(isAll==false && cl[*itr]==cl[u]) isAll = true;
+						}
+					}
+				}
+				if(isAll) TOTAL_ROAD += pnt*(pnt-1)/2-edge;
+				else TOTAL_ROAD += (pnt/2)*(pnt/2+pnt%2)-edge;
+				//cout << " isAll " << isAll << " pnt " << pnt << " edge " << edge << endl;
 			}
 		}
 		cout << TOTAL_ROAD << "\n";
 		for(int i=0;i<N;i++) Alist[i].clear();
 	}
 }
-

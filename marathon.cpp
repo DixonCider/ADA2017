e936a13057e9148c0d65f7b72d683d9ed7ea0e97
zennen <b05502055@csie.ntu.edu.tw>
2018-01-11 21:40:21 +0800

    AC

--- marathon.cpp
+++ marathon.cpp
@@ -1,37 +1,57 @@
 #include <iostream>
 #include <vector>
 #include <string.h>
-#include <stack>
+#include <climits>
 using namespace std;
+
+bool lastStop[40] = {0};
+vector< vector<int> >Alist(40);
+int GLOBALMIN = INT_MAX, dest;
+int c[40] = {0};
+
+void tsp(int here, int cost, vector<bool> &past){
+	vector<bool> visit = past;
+	vector<int>::iterator itr;
+	for(itr=Alist[here].begin();itr!=Alist[here].end();++itr){
+		if(!past[*itr]){
+			// visit now
+			visit[*itr] = true;
+			cost += c[*itr];
+		}
+	}
+	if(cost>GLOBALMIN) return;
+	if(dest == here){
+		GLOBALMIN = cost;
+		return;
+	}
+	if(lastStop[here]){
+		// go to last stop!!!
+		tsp(dest,cost,visit);
+		return;
+	}
+	for(itr=Alist[here].begin();itr!=Alist[here].end();++itr){
+		if(!past[*itr]) tsp(*itr,cost,visit);
+	}
+}
 
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	int N,M,tmp1,tmp2,MIN=-1;
-	int c[40] = {0};
-	bool visited[40] = {0};
+	vector<bool> visit(40,0);
+	int N,M,u,v;
 	cin >> N >> M;
-	vector< vector<int> >Alist(N);
+	dest = N-1;
 	for(int i=0;i<N;++i) cin >> c[i];
 	for(int i=0;i<M;++i){
-		cin >> tmp1 >> tmp2;
-		--tmp1;
-		--tmp2;
-		Alist[tmp2].push_back(tmp1);
-		Alist[tmp1].push_back(tmp2);
+		cin >> u >> v;
+		--u;
+		--v;
+		Alist[u].push_back(v);
+		Alist[v].push_back(u);
+		if(u==dest) lastStop[v] = true;
+		else if(v==dest) lastStop[u] = true;
 	}
-	stack <int> dfs;
-	dfs.push(0);
-	while(!dfs.empty()){
-		int v = dfs.top();
-		dfs.pop();
-		for(vector<int>::iterator itr = Alist[v].begin(); itr!=Alist[v].end(); ++itr){
-			if(visited[*itr]==false){
-				dfs.push(*itr);
-				visited[*itr] = true;
-			}
-		}
-
-	}
+	visit[0] = true;
+	tsp(0,c[0],visit);
+	cout << GLOBALMIN;
 }
--- switches.cpp
+++ switches.cpp
@@ -1,14 +1,97 @@
 #include <iostream>
+#include <cstdlib>
+#include <bitset>
+#include <ctime>
 using namespace std;
+
+char box[3][3002];
+bitset<26> letter;
 
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	srand(time(NULL));
 	int T;
+	char buf;
 	cin >> T;
 	while(T--){
+		letter.reset();
 		int M;
+		int Tpass=0,bestImp,bestBit;
+		bool isPass = false;
+		unsigned long init = 0, ansDig = 0;
 		cin >> M;
-
+		for(int i=0;i<3;++i) cin >> box[i];
+		for(int i=0;i<3*M+1;++i){
+			if(i%3==0) continue;
+			for(int j=0;j<3;++j){
+				buf = box[j][i];
+				//int u = (int) box[j][i];
+				if(buf<=90){
+					//u-=65;
+					letter.set((int)buf-65);
+				}
+				else if(buf>=97){
+				   	//u-=97;
+					letter.set((int)buf-97);
+				}
+				// cout << u << endl;
+			}
+		}
+		bitset<26> ans(init);
+		int itr = 0;
+		while(itr<75){
+			bestImp = 0;
+			bestBit = -1;
+			for(int i=0;i<26;++i){
+				bitset<26>attempt(ans);
+				if(letter[i]){
+					attempt.flip(i);
+					int stat = 0;
+					for(int j=0;j<M;++j){
+						for(int k=0;k<3;++k){
+							buf = box[k][3*j+1];
+							if((buf<=90 && attempt[(int)buf-65])||(buf>=97 && !attempt[(int)buf-97])){
+								buf = box[k][3*j+2];
+							}
+							else continue;
+							if((buf<=90 && attempt[(int)buf-65])||(buf>=97 && !attempt[(int)buf-97])){
+								++stat;
+								break;
+							}
+						}
+					}
+					if(stat>bestImp){
+						bestImp = stat;
+						bestBit = i;
+					}
+				}
+			}
+			// if best improvement is better than the global
+			if(bestImp>Tpass){
+				Tpass = bestImp;
+				ans.flip(bestBit);
+				ansDig = ans.to_ulong();
+				if(Tpass==M){ 
+					isPass = true;
+					break;
+				}
+			}
+			else{
+				++itr;
+				Tpass = 0;
+				// jump
+				for(int i=0;i<26;++i) if(letter[i]) ans[i] = rand()%2;
+			}
+		}
+		bitset<26> print(ansDig);
+		print &= letter;
+		if(isPass){
+			cout << letter.count() - print.count() << " ";
+			for(int i=0;i<26;++i) if(letter[i]&&!print[i]) cout << (char)(i+65) << " ";
+			cout << "\n";
+		}
+		else cout << "-1\n";
 	}
+	return 0;
 }
